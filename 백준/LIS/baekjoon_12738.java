4222536dc6a15259b87186ffe9ba6c63e54bbd18
kwn1996kr <kwn1996kr@naver.com>
2021-03-31 21:29:35 +0900

    algorithm

---
+++ 백준/LIS/baekjoon_12738.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+/*	백준_12738번_가장 긴 증가하는 부분 수열 3
+ *  2021/ 03 / 31
+ */
+public class Main {
+	static int[] arr;
+	static int[] ans;
+	static int binary_search(int start, int end,int target) {
+		int mid;
+		while(start<end) {
+			mid = (start+end)/2;
+			if(ans[mid]<target) {
+				start = mid + 1;
+			}
+			else {
+				end = mid;
+			}
+		}
+		return end;
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		ans = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		ans[0]=arr[0];
+		int index = 0;
+		
+		for(int i=1;i<n;i++) {
+			if(ans[index]<arr[i]) {
+				ans[++index]=arr[i];
+			}
+			else {
+				int temp = binary_search(0,index,arr[i]);
+				ans[temp]=arr[i];
+			}
+		}
+		
+		System.out.println(index+1);
+	}
+}
